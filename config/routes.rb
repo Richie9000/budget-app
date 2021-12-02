034a180909f4f491942649276dfb66ee7deb9845
Ricardo <rr155914@gmail.com>
2021-12-02 16:07:07 -0600

    Add groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'application#index'
-
-  resources :reports
-  resources :groups
-
-  get '/non_authorized', to: 'application#non_authorized'
 end
